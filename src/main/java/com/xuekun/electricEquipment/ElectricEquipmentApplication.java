d06f3075e9a691dad880d68fa2ef4183021921aa
wymoting <pact@PACT-MoTing>
2018-08-14 18:11:45 +0800

    创建项目并添加静态模板页面

---
+++ src/main/java/com/xuekun/electricEquipment/ElectricEquipmentApplication.java
@@ -0,0 +1,12 @@
+package com.xuekun.electricEquipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricEquipmentApplication.class, args);
+	}
+}
